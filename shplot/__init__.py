c2350136cfc8a9d2c7b2a9f23f2951e1a65840d7
Jayanth Koushik <mail@jkoushik.me>
2024-03-08 15:09:12 -0500

    test: add tests for `ShPlot`

--- shplot/__init__.py
+++ shplot/__init__.py
@@ -1,6 +1,5 @@
 # pylint: disable-all
-# type: ignore
 from ._shplot import *
 from ._version import __version__
 
-__all__ = _shplot.__all__
+__all__ = _shplot.__all__  # type: ignore[name-defined]
